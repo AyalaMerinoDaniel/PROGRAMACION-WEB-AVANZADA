a0724a60a651b4d386062932fd16a34db6dbb2dd
AyalaMerinoDaniel <80376361+AyalaMerinoDaniel@users.noreply.github.com>
2022-05-20 16:55:04 -0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-    //This is the same wuith a include
-require_once "Controllers/plantilla.php";
-require_once "Controllers/controlador.php";
-require_once "Models/usuario.php";
-   
-   $p = new PersControlador();
-   $p -> ctrAccion();
-?>
